3254b269f982993ff3eebe985f10e29a15f5bae0
sharathsrinivasa <sharath.srinivasa1@gmail.com>
2020-03-24 18:12:21 -0500

    Updating brew formula for 3.0.0 release.

--- Formula/butterfly.rb
+++ Formula/butterfly.rb
@@ -1,10 +1,10 @@
 class Butterfly < Formula
-  VERSION = "2.4.0"
+  VERSION = "3.0.0"
 
   desc "Application code transformation tool"
   homepage "https://github.com/paypal/butterfly"
   url "https://search.maven.org/remotecontent?filepath=com/paypal/butterfly/butterfly-cli-package/#{VERSION}/butterfly-cli-package-#{VERSION}.zip"
-  sha256 "f1d52aa58b7746703941097ee6b3580546fbc04e2abdd7fa640c3cd992626778"
+  sha256 "3031194c36e173cdd4ce7c9865f121eaabae138c3a6ef2e32ad30525926f80b0"
 
   depends_on :java => "1.7+"
 
